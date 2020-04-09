36d02e64ab0e922d1863345d84026a7b8d8297d0
Julia Miocene <julia.miocene@gmail.com>
2020-04-09 19:43:21 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f33b49c732d73413871b8c8afd2c8041.js"
+  "/precache-manifest.94268a287824f6a5d2488cd656cb146d.js"
 );
 
 self.addEventListener('message', (event) => {
